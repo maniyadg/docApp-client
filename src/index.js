bbb35dcd80d3dd90244ae25d50d0224a9f93eb54
Maniyarasan <92170430+maniyadg@users.noreply.github.com>
2023-02-28 14:12:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from 'react-redux'
